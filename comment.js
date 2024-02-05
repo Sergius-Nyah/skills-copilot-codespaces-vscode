885cfa4aa68899798eebbf96b1a26313e1f9b1f3
@tech-evangelist <74214119+Sergius-Nyah@users.noreply.github.com>
2024-02-05 09:34:20 +0000

    Copilot Third commit

---
+++ comment.js
@@ -0,0 +1,91 @@
+//Comment Web Server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+const { randomBytes } = require('crypto');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content, status: 'pending' });
+
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending',
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post('/events', async (req, res) => {
+  console.log('Event Received:', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
+
+// Path: event-bus.js
+//Event Bus Web Server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  axios.post('http://posts-clusterip-srv:4000/events
